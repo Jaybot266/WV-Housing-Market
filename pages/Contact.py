bde5c0dcec0e0d20b247fae3e60238a1bfe89884
Jayden K <116582896+Jaybot266@users.noreply.github.com>
2026-07-30 12:40:17 -0400

    Add files via upload

---
+++ pages/Contact.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Contact")
+
+st.write("If you have any questions, concerns, or problems with the application then please send an email to the following email address. I will get to you as soon as I can.")
+
+st.divider()
+
+st.write("Jkisne03@rams.shepherd.edu")
